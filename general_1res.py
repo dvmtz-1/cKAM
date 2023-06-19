cd4bcb8b9caffddabb1a9121e096a2081775e9dc
dvmtz-1 <137048680+dvmtz-1@users.noreply.github.com>
2023-06-19 10:53:38 +0100

    cKAM - 1 resonance example

---
+++ general_1res.py
@@ -0,0 +1,269 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Implementation of converse KAM result (MacKay'18) to an integrable
+# magnetic field example: axisymmetrical magnetic field perturbed with
+# 1 helical mode (Kallinikos'14).
+# 
+# # # # # # # # # # # # # # # # # # # # 
+# Input:  'ini.txt' - Parameter file
+# Output: "gRN_{orbits}_{tf}_{epx}_{m}_{n}.txt"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sympy import symbols, sqrt, sin, cos, derive_by_array, Matrix
+from sympy import tensorproduct, tensorcontraction
+from sympy.utilities.lambdify import lambdify
+from scipy.integrate import solve_ivp
+
+from joblib import Parallel, delayed
+
+import time
+import datetime
+import simplejson
+
+start = time.time()
+
+
+
+
+## PRELIMINARIES ##
+def read_data():
+    file1 = open("ini.txt")
+    data = file1.readlines()
+    r = []
+    for line in data:
+        r.append(float(line))
+    file1.close()
+    return r
+
+data= read_data()
+
+π  = np.pi
+
+
+# Cartesian to toroidal coordinates (on a poloidal cross section)
+def  rf(x,y,z) : return np.sqrt((np.sqrt(x**2+y**2)-R0)**2 + z**2)
+def  ψf(x,y,z) : return (np.sqrt(x**2+y**2)-R0)**2 + z**2
+def thf(x,y,z) : return np.arctan2(z,np.sqrt(x**2+y**2)-R0)
+
+# operations
+def dotproduct(A,B) : return tensorcontraction(tensorproduct(A,B),(0,1)) # dot vectors A, B
+def multiply(A,B)   : return tensorcontraction(tensorproduct(A,B),(1,2)) # multiply matrix A with vector B
+def contract(A,B)   : return tensorcontraction(tensorproduct(A,B),(2,3)) # contract 3-form A with vector B
+# 3D Levi-Civita symbol (for volume form)
+def LC(i,j,k) : return(j-i)*(k-i)*(k-j)/2
+
+
+
+
+
+
+## SYSTEM ##
+
+r, th, ph = symbols('r, th, ph')
+Vr, Vth, Vph = symbols('Vr, Vth, Vph')
+
+m = data[3]
+n = data[4]
+w1 = data[5]
+w2 = data[6]
+R0 = data[7]
+
+# Adjoustmends in new coordinates (ψ, \vartheta, phi)
+a = (1 - r/R0)**2    # r = ψ
+rho = 1 / (1/a -  1) # 
+R = R0 * a / (1 - (1-a)**(0.5) * cos(th))
+ge = r - R0**2  # r = ψ
+
+#String formating for labels - - - - -
+ep=data[2]
+if ep < 0.1 and ep >= 0.01:
+    ep1 = '0'+str(int(ep*1000))
+elif ep >= 0.001:
+    ep1 = '00'+str(int(ep*10000))
+else:
+    ep1 = '000'+str(int(ep*100000))
+# - - - - - - - - - - - - - - - - - -
+
+# Magnetic field #
+def B(r,th,ph) :
+    e1 = ep #
+    # B field - # Different scalings
+    Br  = (m*e1*(r**(m/2))*ge*sin(m*th-n*ph))  #/ (r*R) | *R0/R**2
+    Bth = (w1 + 2*w2*r + e1*(r**(m/2))*((ge*m/(2*r)) + 1)*cos(m*th-n*ph)) # / (r*R) | *R0/R**2
+    Bph = 1     # 1 /(R*r) | *R0/R**2
+    return [Br, Bth, Bph]
+
+# flow #
+def f(r,th,ph) : return B(r,th,ph)
+
+# Jacobian #
+def Df(r,th,ph) :
+    k = len(f(r,th,ph))
+    return [derive_by_array(f(r,th,ph)[i],[r,th,ph]) for i in range(0,k)]
+
+# tangent flow #
+def Tf(r,th,ph,Vr,Vth,Vph): return multiply(Df(r,th,ph), [Vr,Vth,Vph])
+
+# whole system #
+def F(r,th,ph,Vr,Vth,Vph) : return [*f(r,th,ph), *Tf(r,th,ph,Vr,Vth,Vph)]
+
+Fnum = lambdify((r,th,ph,Vr,Vth,Vph), F(r,th,ph,Vr,Vth,Vph), 'numpy')
+
+def system(t, X):
+    x1,x2,x3,v1,v2,v3 = X
+    dXdt = Fnum(x1,x2,x3,v1,v2,v3)
+    return dXdt
+
+
+
+
+
+
+## DIRECTION FIELD ξ ##
+
+# Spherical coordinates - - - -- 
+#metric = Matrix([[1, 0, 0],      
+#                 [0, r**2, 0],
+#                 [0, 0, R**2]])
+
+
+metric = Matrix([[1/(2*r), 0, 0],   # Adapted cartesian 
+                 [0, 2*r, 0],       # coordinates
+                 [0, 0, R0**2]])
+
+
+def  J(r,th,ph) : return r
+def dJ(r,th,ph) : return derive_by_array(J(r,th,ph),[r,th,ph])
+
+def xi(r,th,ph) :
+    gradJ = multiply(metric.inv(), dJ(r,th,ph))
+    #gradH = multiply(metric.inv(), dH(x,y,px,py))
+    #dH2  = dotproduct(gradH, dH(x,y,px,py))
+    #dHdJ = dotproduct(gradJ, dH(x,y,px,py))
+    #a = dHdJ/dH2
+    return gradJ #- a*gradH
+
+ξ = lambdify((r,th,ph), xi(r,th,ph), 'numpy')
+
+def mod2ξ(r,th,ph) : return np.dot(ξ(r,th,ph), ξ(r,th,ph))
+
+
+
+
+
+
+## 1-FORM λ ##
+
+def Lambda(r,th,ph) :
+    dJf = dJ(r,th,ph)
+    ff  = multiply(metric, f(r,th,ph))
+    f2  = dotproduct(ff, f(r,th,ph))
+    fdJ = dotproduct(dJf,f(r,th,ph))
+    b = fdJ/f2 
+    return dJf - b*ff
+
+λ = lambdify((r,th,ph), Lambda(r,th,ph), 'numpy')
+
+
+
+
+
+
+## CONVERSE KAM CONDITION ##
+
+# volume form #
+def vol(r,th,ph) :
+    #ρ = r*R  # sqrt(metric.det())
+    #ρ = R*2/R0  #  # David
+    ρ = R0**2  #  # David & Robert
+    v = ρ*np.fromfunction(LC, (3,3,3))
+    return v.tolist()
+
+# magnetic flux form #
+def beta(r,th,ph) : return contract(vol(r,th,ph), B(r,th,ph))
+β = lambdify((r,th,ph), beta(r,th,ph), 'numpy')
+
+def con(t, X) :
+    x1,x2,x3,v1,v2,v3 = X
+    if mod2ξ(x1,x2,x3) > 1e-12 :
+        ω  = np.dot(ξ(x1,x2,x3), np.matmul(β(x1,x2,x3), [v1,v2,v3]))
+        λe = np.dot(λ(x1,x2,x3), [v1,v2,v3])
+        return abs(ω - 1000*λe) + 1000*λe
+con.terminal = True
+
+
+
+
+
+
+
+
+
+## INTEGRATION & RESULTS ##
+
+# time parameters #
+tf = int(data[0])
+t_int = [0, tf]
+t = np.linspace(0, tf, 2001)
+
+
+# space sampling #
+o_ax = int(data[1])  # sample each axis
+orbits = o_ax**2 # grid points
+hr = 1.0         # axis halfrange
+
+y0_list = np.linspace(R0-hr, R0+hr, o_ax)
+z0_list = np.linspace(  -hr,    hr, o_ax)
+
+ph0 = 0
+
+
+
+def loop(i,j) :
+    
+    s = time.time()
+    
+    y0 = y0_list[i] # No longer physical coordinates but
+    z0 = z0_list[j] # x = sqrt(2ψ) cos(vth), y = sqrt(2ψ) sin(vth) 
+    
+
+    r0 = ψf(0,y0,z0)/2
+    th0 = thf(0,y0,z0)  
+        
+    v0 = ξ(r0, th0, ph0)
+    X0 =  [r0, th0, ph0, *v0]
+    
+    SOL = solve_ivp(system, t_int, X0, method='RK45', events=con, rtol=1e-8, atol=1e-11)
+    
+    f = time.time()
+    tr = f-s
+    
+    ie = SOL.status
+    if ie==1 : te = SOL.t_events[0][0]
+    else : te = tf
+    
+    #return y0, z0, ie, te, tr
+    return y0, z0, ie, te, tr, SOL.y[2][-1]  #Extra: phi[-1]
+
+
+## PARALELIZATION ON CPU CORES ##
+if __name__ == "__main__":
+    result = Parallel(n_jobs=-1)(delayed(loop)(i,j) for i in range(o_ax) for j in range(o_ax))
+                           # -1=all cores, 1=serial
+
+
+## RUNNING TIME ##
+finish = time.time()
+hours, rem = divmod(finish-start, 3600)
+minutes, seconds = divmod(rem, 60)
+print('time = {:0>2}:{:0>2}:{:02.0f}'.format(int(hours),int(minutes),seconds))
+
+
+## SAVING DATA ##
+file = open('gRN_%s_%s_%s_%s_%s.txt' % (orbits, tf, ep1,int(m),int(n)), 'w')
+simplejson.dump(result, file)
+file.close()
+
+
+
---
+++ general_plot.py
@@ -0,0 +1,181 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Plot from 'general_1res.py'
+# [Implementation of converse KAM result (MacKay'18) to an integrable
+# magnetic field example: axisymmetrical magnetic field perturbed with
+# 1 helical mode (Kallinikos'14).]
+# 
+# # # # # # # # # # # # # # # # # # # # 
+# Inputs:  'ini.txt' - Parameter file
+#          'gRN_{orbits}_{tf}_{epx}_{m}_{n}.txt'
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import simplejson
+import os
+from matplotlib.colors import LinearSegmentedColormap
+
+
+## SET RUN PARAMETERS ##
+def read_data():
+    file1 = open("ini.txt")
+    data = file1.readlines()
+    r = []
+    for line in data:
+        r.append(float(line))
+    file1.close()
+    return r
+
+data = read_data()
+
+
+tf = int(data[0])
+Pts= int(data[1])
+points = Pts**2 #
+
+
+## IMPORTS DATA ##
+ep=data[2]
+m = data[3]
+n = data[4]
+w1 = data[5]
+w2 = data[6]
+R0 = data[7]
+
+if ep < 0.1 and ep >= 0.01:
+    ep1 = '0'+str(int(ep*1000))
+elif ep >= 0.001:
+    ep1 = '00'+str(int(ep*10000))
+else:
+    ep1 = '000'+str(int(ep*100000))
+    
+## OPENS FILE ##
+file = open('gRN_%s_%s_%s_%s_%s.txt' % (points, tf,ep1, int(m),int(n)), 'r')
+result = simplejson.load(file)
+file.close()
+
+
+
+
+x1p = []
+x2p = []
+
+x1u = []
+x2u = []
+
+x1 = []
+x2 = []
+q  = []
+
+for i in range(len(result)) :
+    x10 = result[i][0] - R0 
+    x20 = result[i][1]
+    ie  = result[i][2]
+    te  = result[i][3]
+    if ie == 1 :
+        x1p.append(x10) 
+        x2p.append(x20)
+    else :
+        x1u.append(x10)
+        x2u.append(x20)
+    x1.append(x10)
+    x2.append(x20)
+    q.append(te/tf)
+
+# - = - = - = - = - = - = - = - = - = # - = - = - = - = - = - = - = - = - = 
+deltaR = 0.005
+deltaQ= 0.005
+eps = ep
+fi=0.0
+# - = - = - = - = - = - = - = - = - = 
+r = np.arange(0, R0, deltaR)
+th = np.arange(-np.pi, np.pi, deltaQ)
+ψ, Q = np.meshgrid(r, th)
+# Hamiltonian - - - - - - - - - - - - 
+H = w1*ψ + w2*ψ**2 + eps*(ψ**(m/2))*(ψ-R0**2)*np.cos(m*Q-n*fi)
+Ht=m*H-n*ψ
+
+if m==2 and n==1:    
+    if eps ==0.007:            #                               m n eps
+        ilvl=[-0.0248,-0.01,0.0,0.05 ,0.1,0.15,0.2,0.3, 0.6] # 2 1 0.007
+        ilvl2=[-0.035,-0.03,-0.026]
+    elif eps == 0.004: 
+        ilvl=[-0.02746,-0.026,-0.01,0.0,0.05 ,0.1,0.15,0.2,0.3, 0.6]
+        ilvl2=[-0.033,-0.03,-0.028]                          # 2 1 0.004
+    else: 
+        ilvl=[-0.029339,-0.022,0.0,0.05,0.1,0.2,0.3, 0.6]    # 2 1 0.002
+        ilvl2=[-0.033,-0.031,-0.03]
+else:
+    ilvl=[-0.122825,-0.12,-0.09,-0.02,0.05,0.1,0.2,0.4]      # 3 2 0.007
+    ilvl2=[-0.135,-0.132,-0.127]                             # 3 2
+
+fig= plt.figure(figsize=(8,8))
+fig2= plt.figure(figsize=(10,8))
+ax= fig.gca()
+ax2= fig2.gca()
+
+
+
+
+
+## PLOTTING ##
+
+# standard Converse KAM plot
+
+ax.plot(x1p, x2p, 'ro', markersize=3)
+ax.plot(x1u, x2u, 'bo', markersize=3)
+ax.set_xlabel('$\~y$', fontsize = 16)
+ax.set_ylabel('$\~z$', fontsize = 16, rotation=0)
+
+ax.set_xlim(- 1.05  , 1.05 )     
+ax.set_ylim(-1.05,1.05)
+
+CS = ax.contour(np.sqrt(2*ψ)*np.cos(Q) , np.sqrt(2*ψ)*np.sin(Q), Ht,levels = ilvl,colors=[(0.9,0.9,1)],
+                alpha=0.9 , linestyles=['solid'])
+CS2 = ax.contour(np.sqrt(2*ψ)*np.cos(Q), np.sqrt(2*ψ)*np.sin(Q), Ht,levels = ilvl2, colors=['yellow'],
+                 alpha=0.9, linestyles=['solid'])
+
+
+
+# Converse-KAM-speed plot - - - - - - - - - - - - 
+## Adapted color-map  - - - - - - - ##
+col_list0 = [(0, 0, 1),
+   ( 0.2752 ,   0.5037 ,   0.9677),
+(    0.2287  ,  0.6360  ,  0.9896),
+ (   0.1382  ,  0.7637   , 0.8945),
+  (  0.0929 ,   0.8690    ,0.7571),
+   ( 0.1732 ,   0.9403,    0.6200),
+    (0.3561 ,   0.9856 ,   0.4452),
+(    0.5574  ,  0.9988  ,  0.2882),
+ (   0.7077,    0.9718   , 0.2099),
+  (  0.8380 ,   0.9022    ,0.2088),
+   ( 0.9394  ,  0.8040,    0.2275),
+(    0.9907   , 0.6892 ,   0.2088),
+ (   0.9917,    0.5416  ,  0.1496),
+  (  0.9527 ,   0.3849   , 0.0822),
+    (1,0,0)]
+myorder = [14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
+col_list = [col_list0[i] for i in myorder]
+cmap_name = 'my_list'
+cm1 = LinearSegmentedColormap.from_list(cmap_name, col_list, N=90)
+## - - - - - - - - - - - - - - - - - ##
+
+sc = ax2.scatter(x1, x2, c=q, vmin=0, vmax=1, s=10, cmap = cm1) #
+fig2.cbar = plt.colorbar(sc)
+fig2.cbar.set_label('$q$', rotation=0, fontsize = 16, labelpad=10, y=0.5) # 
+
+ax2.set_xlabel('$\~y$', fontsize = 16)
+ax2.set_ylabel('$\~z$', fontsize = 16 , rotation=0)
+
+
+ax2.set_xlim(- 1.05 , 1.05 )     
+ax2.set_ylim(-1.05,1.05)
+CS3 = ax2.contour(np.sqrt(2*ψ)*np.cos(Q), np.sqrt(2*ψ)*np.sin(Q), Ht,levels = ilvl,colors=[(0.9,0.9,1)], # 'white'
+                       alpha=0.9 , linestyles=['solid'])
+CS4 = ax2.contour(np.sqrt(2*ψ)*np.cos(Q), np.sqrt(2*ψ)*np.sin(Q), Ht,levels = ilvl2,colors=['yellow'],
+                                 alpha=0.9 , linestyles=['solid'])
+
+
+plt.show()
+
